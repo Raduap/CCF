c36e65a922c7239981f7474f58d28151fc8046fe
Raduap <43844930+Raduap@users.noreply.github.com>
2020-09-08 23:55:19 +0800

    Add files via upload

---
+++ problem3.cpp
@@ -0,0 +1,11 @@
+/*
+考虑一个简单的二分类问题——将二维平面上的点分为A 和B 两类。
+训练数据包含 n 个点，其中第 i 个点（1 ≤ i ≤ n）可以表示为一个三元组  (xi, yi,typei)，即该点的横坐标、纵坐标和类别。
+
+*/
+#include <iostream>
+using namespace std;
+int sb(){
+	cout << "sc";
+	return 0;
+}
